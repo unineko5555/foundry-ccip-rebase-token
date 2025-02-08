2a2923b0d53d9a5c6a69e2ed03a97eac469f9c3c
unineko5555 <yuking04170417@gmail.com>
2025-02-08 17:17:57 +0900

    ✨ Ownable,AccessControl,関数の追加

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.24;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
 /*
 * @title RebaseToken
 * @author unineko
@@ -17,41 +19,70 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 */
 
 
-contract RebaseToken is ERC20 {
+contract RebaseToken is ERC20, Ownable, AccessControl {
     error  RebaseToken__InterestRateCanOnlyDecrease(uint256 oldInterestRate, uint256 newInterestRate);
 
     uint256 private constant PRECISION_FACTOR = 1e18;
+    bytes32 public constant MINT_AND_BURN_ROLE = keccak256("MINT_AND_BURN_ROLE");
     uint256 private s_interestRate = 5e10;
     mapping (address => uint256) private s_userInterestRate;
     mapping (address => uint256) private s_lastUpdatedTimestamp;
 
     event InterestRateSet(uint256 newInterestRate);
 
-    constructor() ERC20("Rebase Token", "RBT") {
+    constructor() ERC20("Rebase Token", "RBT") Ownable(msg.sender) AccessControl() {
         _mint(msg.sender, 1000000 * 10 ** decimals());
+        _setpRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINT_AND_BURN_ROLE, msg.sender);
+    }
+
+    function grantMintAndBurnRole(address _account) external onlyOwner {
+        _grantRole(MINT_AND_BURN_ROLE, _account);
     }
     /*
     * @Deposits tokens into the vault and mints the user tokens
     * @param _newInterestRate The new interest rate to set
     * @dev The interest rate can only decrease
     */
-    function setInterestRate(uint256 _newInterestRate) external {
+    function setInterestRate(uint256 _newInterestRate) external onlyOwner {
         // Set the interest rate
-        if (_newInterestRate > s_interestRate) {
+        if (_newInterestRate < s_interestRate) { //> でない？？
             revert RebaseToken__InterestRateCanOnlyDecrease(s_interestRate, _newInterestRate);
         }
         s_interestRate = _newInterestRate;
         emit InterestRateSet(_newInterestRate);
+    }
+
+    /*
+    * @notice Get the principale balance of the user. This is the number of tokens that have actually been minted to the user, not including any interest that has accrued since the last user interected with the protocol.
+    * @param _user The user to get the balance for
+    * @return The principale balance of the user
+    */
+    function principalebalanceOf(address _user) public view returns (uint256) {
+        return super.balanceOf(_user);
     }
     /*
     * @notice Mint the user tokens when they deposit into the vault
     * @param _to The user to mint the tokens to
     * @param _amount The amount of tokens to mint
     */
-    function mint(address _to, uint256 _amount) external { //ここでsuperを使わない理由は？
+    function mint(address _to, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE){ //ここでsuperを使わない理由は？
         _mintAccruedInterest(_to);
         s_userInterestRate[_to] = s_interestRate;
         _mint(_to, _amount);
+    }
+
+    /*
+    * @notice Burn the user tokens when they withdraw from the vault
+    * @param _from The user to burn the tokens from
+    * @param _amount The amount of tokens to burn
+    */
+    function burn(address _from, uint256 _amount) external onlyRole(MINT_AND_BURN_ROLE) {
+        if (_amount == type(uint256).max) { //AAVEv3で使用されている
+            _amount = balanceOf(_from);
+        }
+        _mintAccruedInterest(_from);
+        _burn(_from, _amount);
     }
 
     /*
@@ -64,6 +95,45 @@ contract RebaseToken is ERC20 {
         // get the current balance of the user(the number of token that have actually been minted to the user)
         // multiply the principle balance by the interest that has accumulated in the time since the balance was last updated
         return super.balanceOf(_user) * _calculateUserAccumulatedInterestSinceLastUpdate(_user) / PRECISION_FACTOR;
+    }
+
+    /*
+    * @notice Transfer tokens from one user to another
+    * @param _recipient The recipient of the tokens
+    * @param _amount The amount of tokens to transfer
+    * @return True if the transfer was successful
+    */
+    function transfer(address _recipient, uint256 _amount) public override returns (bool) {
+        _mintAccruedInterest(msg.sender);
+        _mintAccruedInterest(_recipient);
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(msg.sender);
+        }
+        //tokenを持っていない場合のみ新しいいinterest rateを設定する、すでに持っている場合はinterest rateを変更しない(変更するとinterest  rateを意図的に下げることができるため)
+        if (balanceOf(_recipient) == 0) {
+            s_userInterestRate[_recipient] = s_interestRate;
+        }
+        return super.transfer(_recipient, _amount); //ERC20を継承しているのでsuperを使う
+    }
+
+    /*
+    * @notice Transfer tokens from one user to another
+    * @param _sender The sender of the tokens
+    * @param _recipient The recipient of the tokens
+    * @param _amount The amount of tokens to transfer
+    * @return True if the transfer was successful
+    */
+    function transferFrom(address _sender, address _recipient, uint256 _amount) public override returns (bool) {
+        _mintAccruedInterest(_sender);
+        _mintAccruedInterest(_recipient);
+        if (_amount == type(uint256).max) {
+            _amount = balanceOf(_sender);
+        }
+        //tokenを持っていない場合のみ新しいinterest rateを設定する、すでに持っている場合はinterest rateを変更しない(変更するとinterest  rateを意図的に下げることができるため)
+        if (balanceOf(_recipient) == 0) {
+            s_userInterestRate[_recipient] = s_interestRate;
+        }
+        return super.transferFrom(_sender, _recipient, _amount); //ERC20を継承しているのでsuperを使う
     }
 
     /*
@@ -80,17 +150,33 @@ contract RebaseToken is ERC20 {
         // deposit: 10 tokens
         // interest rate 0.5 tokens per second
         // 10 + (10 * 0.5 * 2)
-        uint256 timeElapsed = block.timestamp - s_userLastUpdatedTimestamp[_user];
+        uint256 timeElapsed = block.timestamp - s_lastUpdatedTimestamp[_user];
         linearInterest = (PRECISION_FACTOR + (s_userInterestRate[_user] * timeElapsed));    
     }
 
+    /*
+    * @notice Mint the accrued interest to the user since the last time they intereacted with the protocol (e.g. burn, mint, transfer)
+    * @param _user The user to mint the accrued interest to
+    */
     function _mintAccruedInterest(address _user) internal view { //view??
         // (1) find their current balance of rebase tokens that have been minted to the user -> principal
+        uint256 previousPrincipalBalance = super.balanceOf(_user); //ERC20を継承しているのでsuperを使う
         // (2) calculate their current balance including any interest -> balanceOf
+        uint256 currentBalance = balanceOf(_user);
         // caluculate the number of tokens that have been minted to the user -> (2) - (1) interest
-        // call _mint to mint the tokens to the user 
+        uint256 balanceIncrease = currentBalance - previousPrincipalBalance;
         // set the user's last updated timestamp 
-        s_userLastUpdatedTimestamp[_user] = block.timestamp;
+        s_lastUpdatedTimestamp[_user] = block.timestamp;
+        // call _mint to mint the tokens to the user 
+        _mint(_user, balanceIncrease); //Interaction of CEI
+    }
+
+    /*
+    * @notice Get the interest rate that is currently set for the smart contract.Any future deposits will receive this interest rate
+    * @return The interest rate
+    */
+    function getInterestRate() public view returns (uint256) {
+        return s_interestRate;
     }
 
     /*
