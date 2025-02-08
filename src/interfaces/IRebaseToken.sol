6f1db219243797902c3435c2fafe624651bb3457
unineko5555 <yuking04170417@gmail.com>
2025-02-08 17:18:42 +0900

    ⚡️ Interfaceの追加

---
+++ src/interfaces/IRebaseToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IRebaseToken {
+    function mint(address _to, uint256 _amount) external;
+    function burn(address _from, uint256 _amount) external;
+    // function setInterestRate(uint256 _newInterestRate) external;
+    // function getInterestRate() external view returns (uint256);
+    // function getPrincipalBalance(address _user) external view returns (uint256);
+}
