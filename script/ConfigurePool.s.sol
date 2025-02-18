1d80ee2d17b3a7d59c82456694b6813a5be4feeb
unineko5555 <yuking04170417@gmail.com>
2025-02-18 23:56:06 +0900

    ✨ Poolスクリプト

---
+++ script/ConfigurePool.s.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {TokenPool} from "@ccip/contracts/src/v0.8/ccip/pools/TokenPool.sol";
+import {RateLimiter} from "@ccip/contracts/src/v0.8/ccip/libraries/RateLimiter.sol";
+
+contract ConfigurePoolScript is Script {
+    function run(
+        address localPool, 
+        uint64 remoteChainSelector,
+        address remotePool,
+        address remoteToken,
+        bool outboundRateLimiterIsEnabled,
+        uint128 outboundRateLimiterCapacity,
+        uint128 outboundRateLimiterRate,
+        bool inboundRateLimiterIsEnabled,
+        uint128 inboundRateLimiterCapacity,
+        uint128 inboundRateLimiterRate
+    ) public {
+        vm.startBroadcast();
+        // bytes[] memory remotePoolAddresses = new bytes[](1);
+        bytes memory remotePoolAddress = abi.encode(remotePool); //githubにはないが修正
+        // remotePoolAddress[0] = abi.encodePacked(remotePool);
+        TokenPool.ChainUpdate[] memory chainsToAdd = new TokenPool.ChainUpdate[](1);
+        chainsToAdd[0] = TokenPool.ChainUpdate({
+            remoteChainSelector: remoteChainSelector,
+            allowed: true, //githubにはないが追加
+            remotePoolAddress: remotePoolAddress,
+            remoteTokenAddress: abi.encode(remoteToken),
+            outboundRateLimiterConfig: RateLimiter.Config({
+                isEnabled: outboundRateLimiterIsEnabled,
+                capacity: outboundRateLimiterCapacity,
+                rate: outboundRateLimiterRate
+            }),
+            inboundRateLimiterConfig: RateLimiter.Config({
+                isEnabled: inboundRateLimiterIsEnabled,
+                capacity: inboundRateLimiterCapacity,
+                rate: inboundRateLimiterRate
+            })
+        });
+        // TokenPool(localPool).applyChainUpdates(new uint64[](0), chainToAdd);
+        TokenPool(localPool).applyChainUpdates(chainsToAdd); //githubにはないが修正
+    }
+}
